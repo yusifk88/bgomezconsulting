76245344859245b6039f236137caabb8d9240f5c
yusifk88 <amloyal4life@gmail.com>
2025-11-18 14:52:40 -0500

    project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bgomez consulting
+This is the source code for the public or client facing area of 
+[bgomezconsulting.com](https://bgomezconsulting.com)
---
+++ resources/js/app.js
@@ -0,0 +1,35 @@
+import '../css/app.css';
+import './bootstrap';
+
+
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, h } from 'vue';
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+
+const vuetify = createVuetify()
+
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue'),
+        ),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .use(vuetify)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
