53a6b693953c803a236216e5a807a5f54e194671
yusifk88 <amloyal4life@gmail.com>
2025-11-29 16:07:28 -0500

    completd request view

---
+++ resources/js/Components/ShowInfor.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+name: "ShowInfor"
+}
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/statusChip.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+name: "statusChip"
+}
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
