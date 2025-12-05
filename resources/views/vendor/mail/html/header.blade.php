011556045bdd9f34439b7f489a04964ecdca04d0
yusifk88 <amloyal4life@gmail.com>
2025-12-05 16:05:49 -0500

    added logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{!! $slot !!}
-@endif
+<img src="https://flobaze.atl1.cdn.digitaloceanspaces.com/public/logo.svg" class="logo" alt="{{config('app.name')}}">
 </a>
 </td>
 </tr>
