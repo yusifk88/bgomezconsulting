ace44b8eee304d7fa192d61eeb39098e0468df99
yusifk88 <amloyal4life@gmail.com>
2025-12-05 16:15:11 -0500

    added logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="https://flobaze.atl1.cdn.digitaloceanspaces.com/public/logo.svg" class="logo" alt="{{config('app.name')}}">
+<img src="https://flobaze.atl1.cdn.digitaloceanspaces.com/public/logo2.png" class="logo" alt="{{config('app.name')}}">
 </a>
 </td>
 </tr>
