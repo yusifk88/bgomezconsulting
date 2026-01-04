290f748818fea68e37a4e8840308d5eb1d2c42e4
yusifk88 <amloyal4life@gmail.com>
2026-01-04 18:48:07 -0500

    updated information

--- lang/en/app.php
+++ lang/en/app.php
@@ -166,7 +166,7 @@ return [
     "make_appointment" => "Make Appointment",
     "itin" => "ITIN Application, Notary Services",
     "tax_call_out" => 'Do your taxes the right way<br v-if="!$vuetify.display.mobile"> with <span style="border-bottom: 5px solid cyan">professional</span> support',
-    "tax_call_out_description" => "Your taxes should be done right and we are here to make sure you get just that and more with a team of professionals with the sole purpose of making sure you and your business can thrive.",
+    "tax_call_out_description" => "Your taxes should be done right and we are here to assure you get that and more. Our team’s sole purpose is making sure you and your business thrive.",
     "itin_description" => "We are experts in ITIN services including application and notary services, we ensure your IRS Form W-7 is done right.",
     "call_to_action" => "Accounting & tax consulting <br>for you & your business",
     "call_to_action_sub" => "Whether you are an individual, a business or a family,<br> we offer different tiers of services that you can choose from",
